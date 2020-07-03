840a69dea2363b3c055fd35769adfa821b975af1
becode <yf_be@yahoo.co.uk>
2020-07-03 09:22:25 +0200

    evolution1

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
 
 document.getElementById('button').addEventListener('click', () => {
     let pokeId = document.getElementById('input').value;
+    //fetch poke by ID or name
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            document.getElementById('pokeId').innerHTML = `ID: ${data.id}`;
-            document.getElementById('name').innerHTML = `Name: ${data.species.name}`;
-            document.getElementById('sprite').innerHTML = `<img id="pokeImg" class="img d-block mx-auto" src="${data.sprites.front_shiny}" alt="pokeImg">`;
-
-            let moves = data.moves.slice(0, 4);
-            moves.forEach(move => {
-                console.log(move.move.name);
-                document.getElementById('moves').innerHTML += `${move.move.name} `;
-            })
-
-            fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeId}`)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                    let baby = data.evolves_from_species.name;
-                    if (baby != null) {
-                        console.log(data.evolves_from_species.name);
-                    }
+            let pokeName = data.species.name;
+            document.getElementById('name').innerHTML = `Name: ${pokeName}`;
+            function displayPokeInfo() {
+                document.getElementById('pokeId').innerHTML = `ID: ${data.id}`;
+                document.getElementById('sprite').innerHTML = `<img id="front" class="img" src="${data.sprites.front_shiny}" alt="frontImg"><img id="back" class="img" src="${data.sprites.back_shiny}" alt="backImg">`;
+                document.getElementById('moves').innerHTML = '';
+                let moves = data.moves.slice(0, 4);
+                moves.forEach(move => {
+                    console.log(move.move.name);
+                    document.getElementById('moves').innerHTML += `<div class="move">${move.move.name}</div> `;
                 })
-                .catch(error => console.error('Poke does not have evolution'));
+            }
+            displayPokeInfo();
 
+            //fetch previous evolution of pokemon
+            document.getElementById('prev').addEventListener('click', () => {
+                fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeName}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data);
+
+                        if (data.evolves_from_species === null) {
+                            document.getElementById('pokeInfo').innerHTML = `<span class="text-capitalize">${pokeName}</span> is the baby.`;
+                        } else {
+                            baby = data.evolves_from_species.name;
+                            console.log(baby);
+                            fetch(`https://pokeapi.co/api/v2/pokemon/${baby}`)
+                                .then(response => response.json())
+                                .then(data => {
+                                    document.getElementById('name').innerHTML = `Name: ${baby}`;
+                                    if (baby === data.species.name) {
+                                        document.getElementById('pokeId').innerHTML = `ID: ${data.id}`;
+                                        document.getElementById('sprite').innerHTML = `<img id="front" class="img" src="${data.sprites.front_shiny}" alt="frontImg"><img id="back" class="img" src="${data.sprites.back_shiny}" alt="backImg">`;
+                                        document.getElementById('moves').innerHTML = '';
+                                        let moves = data.moves.slice(0, 4);
+                                        moves.forEach(move => {
+                                            console.log(move.move.name);
+                                            document.getElementById('moves').innerHTML += `<div class="move">${move.move.name}</div> `;
+                                        })
+                                    }
+                                })
+                                .catch(error => console.error(error))
+                        }
+                    })
+            })
+                .catch(error => console.error(error))
         })
-        .catch(error => console.error(error))
 })
 
